03fbbb3c0be9924309091e14b163023a59fbc083
Varewyck Tom <32243894+VareTom@users.noreply.github.com>
2021-03-28 15:57:46 +0200

    init commit

---
+++ src/App.svelte
@@ -0,0 +1,12 @@
+<script>
+  import Header from './Header.svelte'
+
+</script>
+
+<style></style>
+
+<div class="App">
+    <header class="App-header">
+      <Header></Header>
+    </header>
+  </div>
